ea10c8c31e8645261cafb60e2df7037e200d9b2e
tygerxqt <hi@tygr.dev>
2023-04-17 10:07:19 +0000

    Add primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          50: "#fff",
+          100: "#fafafa",
+          200: "#eaeaea",
+          300: "#999",
+          400: "#888",
+          500: "#666",
+          600: "#444",
+          700: "#333",
+          800: "#111",
+          900: "#000",
+        }
+      }
     },
   },
   plugins: [],
