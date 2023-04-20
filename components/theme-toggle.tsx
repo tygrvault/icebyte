914e48475e019e2a02ceeff9de0a2482e2a02233
Ty Mason <hi@tygr.dev>
2023-04-20 01:11:31 +0100

    Create a theme-toggle and dropdown component

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Laptop, Moon, Sun } from "lucide-react"
+
+export function ThemeToggle() {
+    const { setTheme } = useTheme()
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild className="outline-none">
+                <Button variant="ghost" size="icon">
+                    <Sun className="block text-black dark:hidden" />
+                    <Moon className="hidden dark:block dark:text-white" />
+                    <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                    <Sun className="w-4 h-4 mr-2" />
+                    <span>Light</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    <Moon className="w-4 h-4 mr-2" />
+                    <span>Dark</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                    <Laptop className="w-4 h-4 mr-2" />
+                    <span>System</span>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
