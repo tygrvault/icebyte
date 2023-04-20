f117056f703894ae3296407ea64793fc725f837a
tygerxqt <hi@tygr.dev>
2023-04-20 20:38:57 +0000

    Add the toaster and auth-provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
 import "../styles/globals.css";
 
-import { ThemeProvider } from 'next-themes';
-import Nav from '@/components/nav';
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "next-themes";
+import Nav from "@/components/nav";
+import type { AppProps } from "next/app";
+import { AuthProvider } from "@/components/auth/context";
+import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-
-            <ThemeProvider enableSystem={true} attribute="class">
-                <Nav />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </>
-    );
+  return (
+    <>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Toaster theme="dark" className="h-full min-h-[50px]" />
+        <AuthProvider>
+          <Nav />
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ThemeProvider>
+    </>
+  );
 }
