6850a1f2bd85f21c3f94169f77984b0e73eb5c60
Ty Mason <hi@tygr.dev>
2023-04-23 22:25:19 +0100

    Add tailwind typography and vercel cli

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,5 +30,8 @@ module.exports = {
       }
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('@tailwindcss/typography'),
+  ],
 }
