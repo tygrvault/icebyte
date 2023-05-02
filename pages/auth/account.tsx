1b43dc2d9267c1c7fb0b1b4d97d55e2c7f9f4c27
Ty Mason <hi@tygr.dev>
2023-05-02 01:29:39 +0100

    Create protected page component

--- pages/auth/account.tsx
+++ pages/auth/account.tsx
@@ -1,18 +1,22 @@
+import ProtectedPage from "@/components/auth/ProtectedPage";
+
 export default function AccountPage() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center w-full">
-                <div className="flex flex-col max-w-[1400px] items-start justify-start w-full gap-4 p-4 sm:px-8 sm:pt-4">
-                    <div className="flex-col items-center gap-0">
-                        <h1 className="text-4xl font-bold">
-                            Account
-                        </h1>
-                        <p className="text-primary-500">
-                            Manage your preferences, settings and everything in between.
-                        </p>
+            <ProtectedPage>
+                <div className="flex flex-col items-center justify-center w-full">
+                    <div className="flex flex-col max-w-[1400px] items-start justify-start w-full gap-4 p-4 sm:px-8 sm:pt-4">
+                        <div className="flex-col items-center gap-0">
+                            <h1 className="text-4xl font-bold">
+                                Account
+                            </h1>
+                            <p className="text-primary-500">
+                                Manage your preferences, settings and everything in between.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </ProtectedPage>
         </>
     )
 }
--- pages/auth/profile.tsx
+++ pages/auth/profile.tsx
@@ -1,18 +1,22 @@
+import ProtectedPage from "@/components/auth/ProtectedPage";
+
 export default function ProfilePage() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center w-full">
-                <div className="flex flex-col max-w-[1400px] items-start justify-start w-full gap-4 p-4 sm:px-8 sm:pt-4">
-                    <div className="flex-col items-center gap-0">
-                        <h1 className="text-4xl font-bold">
-                            Profile
-                        </h1>
-                        <p className="text-primary-500">
-                            Manage your style and how you look to others on Icebyte!
-                        </p>
+            <ProtectedPage>
+                <div className="flex flex-col items-center justify-center w-full">
+                    <div className="flex flex-col max-w-[1400px] items-start justify-start w-full gap-4 p-4 sm:px-8 sm:pt-4">
+                        <div className="flex-col items-center gap-0">
+                            <h1 className="text-4xl font-bold">
+                                Profile
+                            </h1>
+                            <p className="text-primary-500">
+                                Manage your style and how you look to others on Icebyte!
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </ProtectedPage>
         </>
     )
 }
