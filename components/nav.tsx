fb1448ac59cd46d2af74a32cf675bbea74520a60
Ty Mason <hi@tygr.dev>
2023-05-06 01:17:34 +0100

    Create a gradient to hide content after nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -11,8 +11,8 @@ import Link from "next/link";
 export default function Nav() {
   return (
     <>
-      <nav className="sticky top-0 flex items-center justify-center">
-        <div className="m-3 sm:mx-6 sm:mt-3  border border-black/10 dark:border-white/10 rounded-xl max-w-[1400px] w-full flex items-center justify-between h-[76px] min-h-[76px] px-2 sm:px-4 md:px-6 backdrop-blur-md bg-white/60 dark:bg-black/60">
+      <nav className="sticky top-0 flex items-center justify-center bg-gradient-to-b from-primary-200 dark:from-primary-800 from-20%">
+        <div className="m-3 sm:mx-6 sm:mt-3 border border-black/10 dark:border-white/10 rounded-xl max-w-[1400px] w-full flex items-center justify-between h-[76px] min-h-[76px] px-2 sm:px-4 md:px-6 backdrop-blur-md bg-white/60 dark:bg-black/60">
           <Link href="/" className="transition-all">
             <Image width={42} height={42} alt="Logo" src={DarkLogo} className="hidden dark:block" />
             <Image width={42} height={42} alt="Logo" src={LightLogo} className="block dark:hidden" />
