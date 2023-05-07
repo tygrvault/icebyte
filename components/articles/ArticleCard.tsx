abc55acf4b1ae2138e1442e1196882e5cfa427be
Ty Mason <hi@tygr.dev>
2023-05-07 18:39:16 +0100

    Create the articles page

---
+++ components/articles/ArticleCard.tsx
@@ -0,0 +1,30 @@
+import { Article } from "@/.contentlayer/generated"
+
+export default function ArticleCard({ article }: { article: Article }) {
+    return (
+        <>
+            <div tabIndex={0} role="button" className="rounded-md relative flex w-full min-h-[550px] border border-black/10 dark:border-white/10 md:hover:-translate-y-1 md:hover:active:translate-y-0 transition-all duration-300">
+                <div className="flex flex-col flex-auto w-full h-auto p-0 text-left ">
+                    <div className="w-full h-full min-w-full overflow-hidden rounded-md ">
+                        <img src={article.image} alt="Article Image" className="object-cover w-full h-full" />
+                    </div>
+                </div>
+                <div className="absolute bottom-0 w-full border-t border-black/10 dark:border-white/10 rounded-b-md backdrop-blur-md bg-black/40">
+                    <div className="flex flex-col items-start justify-center w-full gap-2 p-4">
+                        <div className="flex flex-row items-center justify-between w-full gap-3 ">
+                            <h1 className="text-2xl font-bold text-white">
+                                {article.title}
+                            </h1>
+                            <p className="text-primary-100">
+                                { } min read
+                            </p>
+                        </div>
+                        <p className="text-primary-200 ">
+                            {article.summary}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,6 +1,53 @@
+import { allArticles } from "@/.contentlayer/generated";
+import ArticleCard from "@/components/articles/ArticleCard";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Filter } from "lucide-react";
+import { useEffect, useState } from "react";
+
 export default function ArticlesPage() {
+    const [query, setQuery] = useState("");
+    const [queryAmount, setQueryAmount] = useState(0);
+
+    useEffect(() => {
+        setQueryAmount(allArticles.filter((article) => article.title.toLowerCase().includes(query.toLowerCase()) || article.summary.toLowerCase().includes(query.toLowerCase())).length);
+    }, [query])
+
     return (
         <>
+            <div className="flex items-center justify-center w-full py-4">
+                <div className="flex flex-col items-center gap-4 w-full max-w-[1400px]">
+                    <div className="flex flex-col items-center w-full">
+                        <h1 className="text-4xl font-extrabold">Articles</h1>
+                        <p className="text-primary-500">The entire list of all articles on IceByte.</p>
+                    </div>
+                    <div className="flex flex-row items-center justify-center w-full gap-2 px-16 md:px-0 md:w-1/2 lg:w-1/3">
+                        <Input placeholder="Search..." className="w-full h-9 min-w-1/3" value={query} onChange={(e) => setQuery(e.target.value)} />
+                        <Button size="icon" className="h-9 w-9">
+                            <Filter className="w-4 h-4" />
+                        </Button>
+                    </div>
+                    <div className="flex flex-col w-full px-8 pt-8 lg:px-16 ">
+                        {queryAmount < 1 && (
+                            <div className="flex flex-col items-center justify-center w-full">
+                                <h1 className="text-2xl font-bold">No results found.</h1>
+                                <p className="text-primary-500">Try searching for something else.</p>
+                            </div>
+                        )}
+                    </div>
+                    <div className="grid grid-cols-1 gap-6 px-8 pt-8 lg:px-16 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3">
+                        {allArticles
+                            .filter((article) => article.title.toLowerCase().includes(query.toLowerCase()) || article.summary.toLowerCase().includes(query.toLowerCase()))
+                            .map((article) => {
+                                return (
+                                    <div key={article._id}>
+                                        <ArticleCard article={article} />
+                                    </div>
+                                )
+                            })}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
