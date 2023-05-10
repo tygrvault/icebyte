f35f6845e87163233636d6cde2a3f5316d4caf05
Ty Mason <hi@tygr.dev>
2023-05-10 20:31:07 +0100

    Adjust index page margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,12 +24,12 @@ export default function Home() {
         <meta property="og:image" content="https://icebyte.tygr.dev/og.png" />
         <meta property="og:url" content="https://icebyte.tygr.dev" />
       </Head>
-      <div className="flex flex-col gap-8 justify-center items-center my-12 w-full sm:gap-6 md:gap-4 lg:gap-2 md:my-24 lg:my-32 xl:my-44 2xl:my-54">
+      <div className="flex flex-col items-center justify-center w-full gap-8 my-12 sm:gap-6 md:gap-4 lg:gap-2 md:my-16 lg:my-24 xl:my-32 2xl:my-40">
 
         <div className="flex flex-col items-center max-w-[1400px] justify-center gap-4 text-center md:flex-row xs:gap-2">
           <Image alt="Logo" src={DarkLogo} className="hidden w-48 h-48 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-96 xl:h-96 dark:block" />
           <Image alt="Logo" src={LightLogo} className="block w-48 h-48 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-96 xl:h-96 dark:hidden" />
-          <div className="flex flex-col gap-6 items-center p-4">
+          <div className="flex flex-col items-center gap-6 p-4">
             <div className="flex flex-col gap-2">
               <small className="translate-y-2 sm:text-sm md:text-md lg:text-lg xl:text-lg text-primary-500">
                 Introducing Icebyte...
@@ -41,7 +41,7 @@ export default function Home() {
                 Icebyte is a brand-spanking new review platform for indie games of all sizes.
               </p>
             </div>
-            <div className="flex flex-col gap-2 justify-center items-center w-full sm:flex-row">
+            <div className="flex flex-col items-center justify-center w-full gap-2 sm:flex-row">
               <Search />
               <Link href="/articles" className="hidden sm:block">
                 <Button theme="secondary" variant="shadow" size="sm">
@@ -64,29 +64,29 @@ export default function Home() {
             </h1>
           </Balancer>
           <Balancer>
-          <p className="text-center basis-2/3 md:text-end text-primary-600 dark:text-primary-300 sm:text-start">
-            Brought you by Nord Studio, IceByte is a brand-spanking-new review platform primarily for games from small to medium sized indie games.
-            <br /> <br />
-            We created IceByte on our game division to showcase some of our favourite games which have inspired us to keep thinking differently.
-          </p>
+            <p className="text-center basis-2/3 md:text-end text-primary-600 dark:text-primary-300 sm:text-start">
+              Brought you by Nord Studio, IceByte is a brand-spanking-new review platform primarily for games from small to medium sized indie games.
+              <br /> <br />
+              We created IceByte on our game division to showcase some of our favourite games which have inspired us to keep thinking differently.
+            </p>
           </Balancer>
         </div>
 
         <div className="px-4 max-w-[1200px] sm:px-6 md:px-8 lg:px-16 xl:px-24 flex flex-col items-center justify-between gap-6 mt-16 sm:mt-18 md:mt-24 text-center md:text-start md:flex-row lg:mt-28 xl:mt-36 xs:gap-4">
-        <Balancer>
+          <Balancer>
             <h1 className="text-4xl font-bold text-center basis-1/3 md:hidden">
               What&apos;s the <span className="underline"><i>review</i></span> process?
             </h1>
           </Balancer>
           <Balancer>
-          <p className="text-center basis-2/3 md:text-start text-primary-600 dark:text-primary-300 sm:text-start">
-            A review is broken down into 4 seperate topics: World, Gameplay, Music, Story. <br /> <br />
-            Each one of these topics are scored out of 25, which leads the maxoumum top score to be 100. These numbered scores can alsobe displayed from grade A being the highest and F being the lowest.
-          </p>
+            <p className="text-center basis-2/3 md:text-start text-primary-600 dark:text-primary-300 sm:text-start">
+              A review is broken down into 4 seperate topics: World, Gameplay, Music, Story. <br /> <br />
+              Each one of these topics are scored out of 25, which leads the maxoumum top score to be 100. These numbered scores can alsobe displayed from grade A being the highest and F being the lowest.
+            </p>
           </Balancer>
           <Balancer>
             <h1 className="hidden text-4xl font-bold md:inline-block text-end md:text-5xl lg:text-6xl basis-1/3">
-            What&apos;s the <span className="underline"><i>review</i></span> process?
+              What&apos;s the <span className="underline"><i>review</i></span> process?
             </h1>
           </Balancer>
         </div>
